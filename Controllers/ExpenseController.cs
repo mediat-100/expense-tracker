6f7bff6e7b638351e193ffcb32a144750c5c4378
mediat-100 <yusuftomiwa740@gmail.com>
2023-01-18 23:47:56 +0100

    added expense controller

---
+++ Controllers/ExpenseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace expense_tracker.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
